79aa8e10176317b84a3acf8be0955fede2fa8cb1
qilei123 <qileimail123@gmail.com>
2019-04-15 14:35:10 -0400

    commit all

--- mmdet/datasets/extra_aug.py
+++ mmdet/datasets/extra_aug.py
@@ -95,6 +95,7 @@ class RandomCrop(object):
                  min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
                  min_crop_size=0.3):
         # 1: return ori img
+        print min_ious
         self.sample_mode = (1, *min_ious,0)
         self.min_crop_size = min_crop_size
 
