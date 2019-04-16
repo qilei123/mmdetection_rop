a95d961b5ed1df0ce53633f3ac799fd16b3399f2
qilei123 <qileimail123@gmail.com>
2019-04-16 11:24:05 -0400

    commit all

--- mmdet/datasets/coco.py
+++ mmdet/datasets/coco.py
@@ -21,13 +21,14 @@ class CocoDataset(CustomDataset):
                'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock',
                'vase', 'scissors', 'teddy_bear', 'hair_drier', 'toothbrush')
     '''
-    #CLASSES = ('Macula','OpticDisk')
-    
+
+    CLASSES = ('Macula','OpticDisk')
+    '''
     CLASSES = {'Laser Photocoagulation Spot','artifact','bleeding',
                 'Stage 1: demarcation line','Stage 2: ridge',
                 'Stage 3: ridge with neovascularization',
                 'proliferation','Retina detachment','carcinoma'}
-    
+    '''
     def load_annotations(self, ann_file):
         self.coco = COCO(ann_file)
         self.cat_ids = self.coco.getCatIds()
