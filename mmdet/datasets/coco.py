67650e0f244da1331439094e1d4996252c319cc4
qilei123 <qileimail123@gmail.com>
2019-04-19 15:53:37 -0400

    commit all

--- mmdet/datasets/coco.py
+++ mmdet/datasets/coco.py
@@ -22,7 +22,7 @@ class CocoDataset(CustomDataset):
                'vase', 'scissors', 'teddy_bear', 'hair_drier', 'toothbrush')
     '''
 
-    CLASSES = ('Macula','OpticDisk')
+    CLASSES = ('OpticDisk','Macula')
     '''
     CLASSES = {'Laser Photocoagulation Spot','artifact','bleeding',
                 'Stage 1: demarcation line','Stage 2: ridge',
