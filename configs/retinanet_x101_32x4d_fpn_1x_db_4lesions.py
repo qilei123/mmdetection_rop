75e640d8427885ef1d0786e00ff612804f4acdf8
qilei123 <qileimail123@gmail.com>
2019-04-19 22:53:06 -0400

    commit all

--- configs/retinanet_x101_32x4d_fpn_1x_db_4lesions.py
+++ configs/retinanet_x101_32x4d_fpn_1x_db_4lesions.py
@@ -1,7 +1,7 @@
 # model settings
 model = dict(
     type='RetinaNet',
-    pretrained='open-mmlab://resnext101_32x4d',
+    pretrained='open-mmlab://resnext101_64x4d',
     backbone=dict(
         type='ResNeXt',
         depth=101,
