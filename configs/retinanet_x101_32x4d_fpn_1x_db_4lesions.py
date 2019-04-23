4a65c61e8b3654f46202d4b6b38ea5e29db6e0d9
qilei123 <qileimail123@gmail.com>
2019-04-23 14:31:54 -0400

    commit all

--- configs/retinanet_x101_32x4d_fpn_1x_db_4lesions.py
+++ configs/retinanet_x101_32x4d_fpn_1x_db_4lesions.py
@@ -53,7 +53,8 @@ test_cfg = dict(
     max_per_img=100)
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/data0/qilei_chen/AI_EYE/BostonAI4DB9/'
+#data_root = '/data0/qilei_chen/AI_EYE/BostonAI4DB9/'
+data_root = '/media/cql/DATA1/data/DR_4LESIONS/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
