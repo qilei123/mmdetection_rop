ba6a81f728a0a73f1ecd2372fd597dba2bf890f8
qilei123 <qileimail123@gmail.com>
2019-04-25 16:24:31 -0400

    commit all

--- tools/dist_train_DR_4lesions.sh
+++ tools/dist_train_DR_4lesions.sh
@@ -2,4 +2,4 @@
 
 PYTHON=${PYTHON:-"python3"}
 
-$PYTHON -m torch.distributed.launch --master_addr='127.0.0.1' --master_port=8801 --nproc_per_node=$2 $(dirname "$0")/train.py $1 --launcher pytorch ${@:3}
+$PYTHON -m torch.distributed.launch --master_addr='127.0.0.1' --master_port=9901 --nproc_per_node=$2 $(dirname "$0")/train.py $1 --launcher pytorch ${@:3}
