da0b16a36ae6450e11d4d6e9fa1bc454c293bff0
qilei123 <qileimail123@gmail.com>
2019-04-25 13:36:22 -0400

    commit all

--- inference_single_img.py
+++ inference_single_img.py
@@ -2,6 +2,7 @@ import mmcv
 from mmcv.runner import load_checkpoint
 from mmdet.models import build_detector
 from mmdet.apis import inference_detector, show_result
+import cv2
 
 cfg = mmcv.Config.fromfile('configs/mask_rcnn_x101_64x4d_fpn_1x_2tissues.py')
 cfg.model.pretrained = None
@@ -15,6 +16,9 @@ _ = load_checkpoint(model, model_dir)
 # test a single image
 img_dir = '/media/cql/DATA1/data/dr_2stages_samples/wrong_samples/0/141_left.jpeg'
 img = mmcv.imread(img_dir)
+resize_scale = 0.4
+height, width, depth = img.shape
+img = cv2.resize(img,(int(resize_scale*width),int(resize_scale*height)))
 result = inference_detector(model, img, cfg)
 show_result(img, result)
 
