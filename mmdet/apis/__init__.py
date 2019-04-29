1fefbcebbd7b4970aae9f8f8f5eee36d9e3dee9f
qilei123 <qileimail123@gmail.com>
2019-04-29 13:13:16 -0400

    commit all

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -1,8 +1,8 @@
 from .env import init_dist, get_root_logger, set_random_seed
 from .train import train_detector
-from .inference import inference_detector, show_result
+from .inference import inference_detector, show_result,show_single_category_result,show_result_rop_2tissue
 
 __all__ = [
     'init_dist', 'get_root_logger', 'set_random_seed', 'train_detector',
-    'inference_detector', 'show_result'
+    'inference_detector', 'show_result','show_single_category_result','show_result_rop_2tissue'
 ]
