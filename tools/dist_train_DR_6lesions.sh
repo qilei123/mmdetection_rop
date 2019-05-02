0f211988f14dc9d76ed2f32d1252dc08ae94a182
qilei123 <qileimail123@gmail.com>
2019-05-02 17:54:49 -0400

    commit all

---
+++ tools/dist_train_DR_6lesions.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+PYTHON=${PYTHON:-"python3"}
+
+$PYTHON -m torch.distributed.launch --master_addr='127.0.0.1' --master_port=9801 --nproc_per_node=$2 $(dirname "$0")/train.py $1 --launcher pytorch ${@:3}
