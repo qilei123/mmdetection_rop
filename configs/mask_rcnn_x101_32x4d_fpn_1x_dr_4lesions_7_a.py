4e28f44c50c98d3f59c58777cfa49a56b5016357
qilei123 <qileimail123@gmail.com>
2019-05-09 14:00:01 -0400

    commit all

--- configs/mask_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a.py
+++ configs/mask_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a.py
@@ -107,7 +107,7 @@ data_root = '/data0/qilei_chen/AI_EYE/BostonAI4DB7_a/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
-    imgs_per_gpu=2,
+    imgs_per_gpu=1,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
