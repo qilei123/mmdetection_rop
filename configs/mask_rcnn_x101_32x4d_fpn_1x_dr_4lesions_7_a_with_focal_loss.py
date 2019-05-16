4aebcbc33eec0aea4e0ed9784c70d03f08191a4c
qilei123 <qileimail123@gmail.com>
2019-05-16 15:34:42 -0400

    commit all

--- configs/mask_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss.py
+++ configs/mask_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss.py
@@ -107,10 +107,10 @@ dataset_type = 'CocoDataset'
 data_root = '/data0/qilei_chen/AI_EYE/BostonAI4DB7_a/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-img_scale_ = (1400, 1400)
+img_scale_ = (1500, 1500)
 data = dict(
     imgs_per_gpu=1,
-    workers_per_gpu=2,
+    workers_per_gpu=1,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2014.json',
