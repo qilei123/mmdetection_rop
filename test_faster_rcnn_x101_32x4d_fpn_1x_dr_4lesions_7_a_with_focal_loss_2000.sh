fb123e15f457e78701966f7db791e090f3a35831
qilei123 <qileimail123@gmail.com>
2019-05-21 17:26:13 -0400

    commit all

--- test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_2000.sh
+++ test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_2000.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0
-python3 tools/test.py configs/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_2000.py /data0/qilei_chen/AI_EYE/BostonAI4DB7_a/work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_2000/epoch_15.pth --gpus 1 --out /data0/qilei_chen/AI_EYE/BostonAI4DB7_a/work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_2000/e15_results.pkl --eval bbox
+python3 tools/test.py configs/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_2000.py /data0/qilei_chen/AI_EYE/BostonAI4DB7_a/work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_2000/epoch_15.pth --gpus 1 --out /data0/qilei_chen/AI_EYE/BostonAI4DB7_a/work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_2000/e15_results.pkl --eval bbox --show
