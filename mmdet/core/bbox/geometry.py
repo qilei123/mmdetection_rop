bf04c5fdb56e371a2ad878748f40fb828c398d8c
qilei123 <qileimail123@gmail.com>
2019-05-21 00:09:25 -0400

    commit all

--- mmdet/core/bbox/geometry.py
+++ mmdet/core/bbox/geometry.py
@@ -123,11 +123,11 @@ def bbox_overlaps2(bboxes1, bboxes2, mode='iou', is_aligned=False):
 
             centers_in_gt = torch.zeros(rows,cols)
             centers = torch.cat((((bboxes2[:,0]+bboxes2[:,2])/2).reshape(-1,1),((bboxes2[:,1]+bboxes2[:,3])/2).reshape(-1,1)),dim=1)
-            centers = centers.repeat(rows)
+            centers = centers.repeat(rows,cols,2)
             print(centers.size())
             print(bboxes1.size())
         
-            print(bboxes1[:,0]<centers[:,0])
+            print(bboxes1[:,0]<centers[0,:,0])
             '''
             ####
             bboxes_center = [(bboxes2[:,0]+bboxes2[:,2])/2,(bboxes2[:,1]+bboxes2[:,3])/2]
