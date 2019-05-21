ffc6e7a87bec2b0df7cf705790abd53c16c5d78a
qilei123 <qileimail123@gmail.com>
2019-05-21 00:14:13 -0400

    commit all

--- mmdet/core/bbox/geometry.py
+++ mmdet/core/bbox/geometry.py
@@ -127,7 +127,7 @@ def bbox_overlaps2(bboxes1, bboxes2, mode='iou', is_aligned=False):
             print(centers.size())
             print(bboxes1.size())
         
-            print(bboxes1[:,0]<centers[0,:,0])
+            print(bboxes1[:,0]<centers[:,0,0])
             '''
             ####
             bboxes_center = [(bboxes2[:,0]+bboxes2[:,2])/2,(bboxes2[:,1]+bboxes2[:,3])/2]
