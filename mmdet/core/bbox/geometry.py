6e640d3e9c432d8303e91ec09c552fcb11f43cb7
qilei123 <qileimail123@gmail.com>
2019-05-21 01:14:57 -0400

    commit all

--- mmdet/core/bbox/geometry.py
+++ mmdet/core/bbox/geometry.py
@@ -128,9 +128,10 @@ def bbox_overlaps2(bboxes1, bboxes2, mode='iou', is_aligned=False):
             print(centers)
             bboxes1_gt = bboxes1.repeat(cols,1,1)
             bboxes1_gt = bboxes1_gt.permute(1,0,2)
+            print(bboxes1_gt)
             print(centers.size())
             print(bboxes1_gt.size())
-        
+            
             print(bboxes1_gt[:,:,0]<centers[:,:,0] + bboxes1_gt[:,:,2]>centers[:,:,0])
             '''
             ####
