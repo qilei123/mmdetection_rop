fec498fd94dcebe1c5145ebb38402a24fcde72a2
qilei123 <qileimail123@gmail.com>
2019-05-23 12:03:03 -0400

    commit all

--- test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_2000_debug.sh
+++ test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_2000_debug.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 python3 tools/test.py configs/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_2000_debug.py /data0/qilei_chen/AI_EYE/BostonAI4DB7_a/work_dirs/faster_rcnn_r50_fpn_1x_2000_debug/epoch_15.pth --gpus 1 --out /data0/qilei_chen/AI_EYE/BostonAI4DB7_a/work_dirs/faster_rcnn_r50_fpn_1x_2000_debug/e15_results.pkl --eval bbox --show
