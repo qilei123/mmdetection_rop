f995d6de706590efe7300f8e717652a1b11e71ff
qilei123 <qileimail123@gmail.com>
2019-05-31 18:01:56 -0400

    commit all

--- configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_with_focal_loss.py
+++ configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_with_focal_loss.py
@@ -101,7 +101,7 @@ data = dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2014.json',
         img_prefix=data_root + 'train2014/',
-        img_scale=(1333, 900),
+        img_scale=(1333, 800),
         img_norm_cfg=img_norm_cfg,
         size_divisor=32,
         flip_ratio=0.5,
@@ -112,7 +112,7 @@ data = dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_val2014.json',
         img_prefix=data_root + 'val2014/',
-        img_scale=(1333, 900),
+        img_scale=(1333, 800),
         img_norm_cfg=img_norm_cfg,
         size_divisor=32,
         flip_ratio=0,
@@ -123,7 +123,7 @@ data = dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_val2014.json',
         img_prefix=data_root + 'val2014/',
-        img_scale=(1333, 900),
+        img_scale=(1333, 800),
         img_norm_cfg=img_norm_cfg,
         size_divisor=32,
         flip_ratio=0,
@@ -150,10 +150,10 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 12
+total_epochs = 20
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = data_root+'work_dirs/faster_rcnn_r50_fpn_1x'
+work_dir = data_root+'work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss'
 load_from = None
 resume_from = None#'/data0/qilei_chen/AI_EYE/BostonAI4DB9/work_dirs/faster_rcnn_r50_fpn_1x/epoch_2.pth'
 workflow = [('train', 1)]
--- tools/test.py
+++ tools/test.py
@@ -24,7 +24,7 @@ def single_test(model, data_loader, show=False):
         #print(dataset) 
         #print(result)
         if show:
-            model.module.show_result(data, result, dataset.img_norm_cfg,score_thr = 0.3)
+            model.module.show_result(data, result, dataset.img_norm_cfg,score_thr = 0.2)
                                     #,dataset=dataset.CLASSES)
 
         batch_size = data['img'][0].size(0)
