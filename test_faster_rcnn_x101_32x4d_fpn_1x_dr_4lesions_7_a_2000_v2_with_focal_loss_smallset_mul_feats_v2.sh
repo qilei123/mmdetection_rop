24c52a90f85906553cffaf099da6a8fb40639a62
qilei123 <qileimail123@gmail.com>
2019-05-31 11:23:23 -0400

    commit all

--- test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_2000_v2_with_focal_loss_smallset_mul_feats_v2.sh
+++ test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_2000_v2_with_focal_loss_smallset_mul_feats_v2.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64
 export CUDA_VISIBLE_DEVICES=0
-python3 tools/test.py configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_2000_v2_with_focal_loss_smallset_mul_feats_v2.py /data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_2000_v2_with_focal_loss_smallset_mul_feats_v2/epoch_12.pth --gpus 1 --out /data0/qilei_chen/AI_EYE/BostonAI4DB7_a/work_dirs/faster_rcnn_r50_fpn_1x_2000_v2_with_focal_loss_smallset_mul_feats_v2/e12_results.pkl --eval bbox
+python3 tools/test.py configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_2000_v2_with_focal_loss_smallset_mul_feats_v2.py /data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_2000_v2_with_focal_loss_smallset_mul_feats_v2/epoch_12.pth --gpus 1 --out /data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_2000_v2_with_focal_loss_smallset_mul_feats_v2/e12_results.pkl --eval bbox
