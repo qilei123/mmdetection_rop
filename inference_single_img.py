0316338b608cb59cb2df436c64b0c8a9a172bf8e
qilei123 <qileimail123@gmail.com>
2019-06-03 15:42:07 -0400

    commit all

--- inference_single_img.py
+++ inference_single_img.py
@@ -65,9 +65,9 @@ result = inference_detector(model, img, cfg)
 act_gpu = activation['conv1'].squeeze()
 act = act_gpu.cpu().numpy()
 print(act.shape)
-fig, axarr = plt.subplots(act.size(0))
-for idx in range(act.size(0)):
-    axarr[idx].imshow(act[idx].squeeze())
+fig, axarr = plt.subplots(act.shape(0))
+for idx in range(act.shape(0)):
+    axarr[idx].imshow(act[idx])
 
 show_single_category_result(img, result,score_thr = args.score_thr,category_id=args.single_category_id,out_file=save_dir+str(time.time())+'_show_single_label_result.jpg')
 show_result(img, result,score_thr = args.score_thr,out_file=save_dir+str(time.time())+'_show_result.jpg')
