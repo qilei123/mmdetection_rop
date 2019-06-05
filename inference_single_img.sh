f74d7d4efb4e271ba0c5292f9c0d927ceb467ee2
qilei123 <qileimail123@gmail.com>
2019-06-05 18:34:30 -0400

    commit all

--- inference_single_img.sh
+++ inference_single_img.sh
@@ -1,3 +1,4 @@
+export CUDA_VISIBLE_DEVICES=1
 git pull
 img_dir=/data0/qilei_chen/AI_EYE/BostonAI4DB7/val2014/flip_val_675_ZJEYY01-S40rec40-03-ddef6826-6ecb-4ac5-a69f-d83c4da28f3b.jpg
 config_dir=configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_bigset.py
