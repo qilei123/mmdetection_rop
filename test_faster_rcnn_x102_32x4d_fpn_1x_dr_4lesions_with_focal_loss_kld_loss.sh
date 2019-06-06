7ed75025b9fd54a82d56dfac2717b5c853c8a3b4
qilei123 <qileimail123@gmail.com>
2019-06-06 14:34:04 -0400

    commit all

---
+++ test_faster_rcnn_x102_32x4d_fpn_1x_dr_4lesions_with_focal_loss_kld_loss.sh
@@ -0,0 +1,15 @@
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64
+export CUDA_VISIBLE_DEVICES=1
+
+config_dir=configs/faster_rcnn_dr_4lesions/faster_rcnn_x102_32x4d_fpn_1x_dr_4lesions_with_focal_loss_kld_loss.py
+model_dir=/data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_kld_loss
+
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_7.pth --gpus 1 --out ${model_dir}/e7_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_8.pth --gpus 1 --out ${model_dir}/e8_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_9.pth --gpus 1 --out ${model_dir}/e9_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_10.pth --gpus 1 --out ${model_dir}/e10_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_11.pth --gpus 1 --out ${model_dir}/e11_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_12.pth --gpus 1 --out ${model_dir}/e12_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_13.pth --gpus 1 --out ${model_dir}/e13_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_14.pth --gpus 1 --out ${model_dir}/e14_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_15.pth --gpus 1 --out ${model_dir}/e15_results.pkl --eval bbox
