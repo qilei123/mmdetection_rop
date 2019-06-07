61fd502a6361a773c57564b3334020351e12b0a7
qilei123 <qileimail123@gmail.com>
2019-06-07 17:16:07 -0400

    commit all

--- inference_single_img.py
+++ inference_single_img.py
@@ -111,7 +111,7 @@ model.backbone.conv1.register_forward_hook(get_activation('conv1'))
 
 resize_scale = args.resize_scale
 
-folders = ['4']
+folders = ['3']
 dataset_dir = '/data0/qilei_chen/AI_EYE/kaggle_data/dataset_4stages/train_4/'
 #folders = ['0']
 #dataset_dir = '/data0/qilei_chen/AI_EYE/kaggle_data/val_binary/'
