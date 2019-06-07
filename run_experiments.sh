06f2360d9d12aa72d9c906bf75aa4542b9091a44
qilei123 <qileimail123@gmail.com>
2019-06-07 13:50:07 -0400

    commit all

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=0,1
-config_file=configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_2000_v2_with_focal_loss_smallset.py
+config_file=configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_2000_v2_with_focal_loss_smallset_pool_stride_5_v1.py
 sh tools/dist_train_DR_4lesions_3000.sh ${config_file} 2
