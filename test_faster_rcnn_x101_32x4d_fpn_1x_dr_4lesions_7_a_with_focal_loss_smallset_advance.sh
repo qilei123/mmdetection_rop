011cba5818e9881409d46fd648b09f15fe8ed991
qilei123 <qileimail123@gmail.com>
2019-06-21 15:16:35 -0400

    commit all

--- test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance.sh
+++ test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance.sh
@@ -9,7 +9,7 @@ python3 tools/test.py ${config_dir} ${model_dir}/epoch_8.pth --gpus 1 --out ${mo
 python3 tools/test.py ${config_dir} ${model_dir}/epoch_9.pth --gpus 1 --out ${model_dir}/e9_results.pkl --eval bbox
 python3 tools/test.py ${config_dir} ${model_dir}/epoch_10.pth --gpus 1 --out ${model_dir}/e10_results.pkl --eval bbox
 python3 tools/test.py ${config_dir} ${model_dir}/epoch_11.pth --gpus 1 --out ${model_dir}/e11_results.pkl --eval bbox
-#python3 tools/test.py ${config_dir} ${model_dir}/epoch_12.pth --gpus 1 --out ${model_dir}/e12_results.pkl --eval bbox
-#python3 tools/test.py ${config_dir} ${model_dir}/epoch_13.pth --gpus 1 --out ${model_dir}/e13_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_12.pth --gpus 1 --out ${model_dir}/e12_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_13.pth --gpus 1 --out ${model_dir}/e13_results.pkl --eval bbox
 #python3 tools/test.py ${config_dir} ${model_dir}/epoch_14.pth --gpus 1 --out ${model_dir}/e14_results.pkl --eval bbox
 #python3 tools/test.py ${config_dir} ${model_dir}/epoch_15.pth --gpus 1 --out ${model_dir}/e15_results.pkl --eval bbox
