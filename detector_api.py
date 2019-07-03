90bab0fcdd47802e2baa0235021db2d999ca53f1
qilei123 <qileimail123@gmail.com>
2019-07-03 00:33:12 -0400

    commit all

--- detector_api.py
+++ detector_api.py
@@ -28,7 +28,7 @@ class lesion_detector():
         self.json_result = None
         self.cfg = None
         self.model = None
-        self.threshold = 0.2
+        self.threshold = 0.3
     def init_predictor(self,config_dir='',model_dir=''):
         self.cfg = mmcv.Config.fromfile(config_dir)
         self.cfg.model.pretrained = None
