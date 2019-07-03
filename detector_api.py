06f87b5a0d39d261efa909e42e480351dc3a6c27
qilei123 <qileimail123@gmail.com>
2019-07-03 00:30:05 -0400

    commit all

--- detector_api.py
+++ detector_api.py
@@ -28,7 +28,7 @@ class lesion_detector():
         self.json_result = None
         self.cfg = None
         self.model = None
-        self.threshold = 0.3
+        self.threshold = 0.2
     def init_predictor(self,config_dir='',model_dir=''):
         self.cfg = mmcv.Config.fromfile(config_dir)
         self.cfg.model.pretrained = None
@@ -59,7 +59,7 @@ class lesion_detector():
             image = cv2.imread(img_dir)
             for result in json_result['results']:
                 bbox = [int(result['bbox'][0]),int(result['bbox'][1]),int(result['bbox'][2]),int(result['bbox'][3])]
-                cv2.rectangle(image,(bbox[0],bbox[1]),(bbox[0]+bbox[2],bbox[1]+bbox[3]),(0,255,0),5)
+                cv2.rectangle(image,(bbox[0],bbox[1]),(bbox[0]+bbox[2],bbox[1]+bbox[3]),(0,255,0),2)
                 cv2.putText(image,str(result['label']),(bbox[0]+bbox[2],bbox[1]),cv2.FONT_HERSHEY_SIMPLEX, 1,(0,255,0),2,cv2.LINE_AA)                
             cv2.imwrite(show_save_dir,image)
             cv2.imshow('test',image)
@@ -68,16 +68,18 @@ class lesion_detector():
         return self.json_result
     def getResult(self):
         return self.json_result
-
+import glob
 def test():
     LesionDetector = lesion_detector()
     config_dir = 'configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1.py'
     model_dir = '/data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_smallset_advance_optdataset4/epoch_9.pth'
     LesionDetector.init_predictor(config_dir,model_dir)
-    img_dir = '/data0/qilei_chen/Development/Datasets/KAGGLE_DR/train/4/5304_right.jpeg'
+    img_dir = '/data0/qilei_chen/Development/Datasets/KAGGLE_DR/train/1/*.jpeg'
     show_save_dir = '/data0/qilei_chen/Development/test_pytorch_detector.jpg'
-    show_save_dir = ''
-    for i in range(10000):
+    #show_save_dir = ''
+    img_dirs = glob.glob(img_dir)
+    #for i in range(10000):
+    for img_dir in img_dirs:
         oldtime=datetime.datetime.now()
         LesionDetector.prediction(img_dir,show_save_dir)
         newtime=datetime.datetime.now()
