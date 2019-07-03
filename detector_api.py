a77a0c23fb62046efc1e5ceedf177796c5982966
qilei123 <qileimail123@gmail.com>
2019-07-03 01:02:04 -0400

    commit all

--- detector_api.py
+++ detector_api.py
@@ -1,3 +1,5 @@
+
+# -*- coding:utf-8 -*-
 import json
 import argparse
 import mmcv
@@ -12,7 +14,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import datetime
-
+import numpy as np
 def xyxy2xywh(bbox):
     _bbox = bbox.tolist()
     return [
@@ -21,6 +23,138 @@ def xyxy2xywh(bbox):
         _bbox[2] - _bbox[0] + 1,
         _bbox[3] - _bbox[1] + 1,
     ]
+
+
+
+def py_cpu_nms(dets, thresh):  
+    """Pure Python NMS baseline."""  
+    x1 = dets[:, 0]  
+    y1 = dets[:, 1]  
+    x2 = dets[:, 2]  
+    y2 = dets[:, 3]  
+    scores = dets[:, 4]  #bbox打分
+  
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)  
+    #打分从大到小排列，取index  
+    order = scores.argsort()[::-1]  
+    #keep为最后保留的边框  
+    keep = []  
+    while order.size > 0:  
+    #order[0]是当前分数最大的窗口，肯定保留  
+        i = order[0]  
+        keep.append(i)  
+        #计算窗口i与其他所有窗口的交叠部分的面积
+        xx1 = np.maximum(x1[i], x1[order[1:]])  
+        yy1 = np.maximum(y1[i], y1[order[1:]])  
+        xx2 = np.minimum(x2[i], x2[order[1:]])  
+        yy2 = np.minimum(y2[i], y2[order[1:]])  
+  
+        w = np.maximum(0.0, xx2 - xx1 + 1)  
+        h = np.maximum(0.0, yy2 - yy1 + 1)  
+        inter = w * h  
+        #交/并得到iou值  
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)  
+        #inds为所有与窗口i的iou值小于threshold值的窗口的index，其他窗口此次都被窗口i吸收  
+        inds = np.where(ovr <= thresh)[0]  
+        #order里面只保留与窗口i交叠面积小于threshold的那些窗口，由于ovr长度比order长度少1(不包含i)，所以inds+1对应到保留的窗口
+        order = order[inds + 1]  
+  
+    return keep
+
+
+
+def py_cpu_softnms(dets, sc, Nt=0.3, sigma=0.5, thresh=0.001, method=2):
+    """
+    py_cpu_softnms
+    :param dets:   boexs 坐标矩阵 format [y1, x1, y2, x2]
+    :param sc:     每个 boxes 对应的分数
+    :param Nt:     iou 交叠门限
+    :param sigma:  使用 gaussian 函数的方差
+    :param thresh: 最后的分数门限
+    :param method: 使用的方法
+    :return:       留下的 boxes 的 index
+    """
+
+    # indexes concatenate boxes with the last column
+    N = dets.shape[0]
+    indexes = np.array([np.arange(N)])
+    dets = np.concatenate((dets, indexes.T), axis=1)
+
+    # the order of boxes coordinate is [y1,x1,y2,x2]
+    y1 = dets[:, 0]
+    x1 = dets[:, 1]
+    y2 = dets[:, 2]
+    x2 = dets[:, 3]
+    scores = sc
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+    for i in range(N):
+        # intermediate parameters for later parameters exchange
+        tBD = dets[i, :].copy()
+        tscore = scores[i].copy()
+        tarea = areas[i].copy()
+        pos = i + 1
+
+        #
+        if i != N-1:
+            maxscore = np.max(scores[pos:], axis=0)
+            maxpos = np.argmax(scores[pos:], axis=0)
+        else:
+            maxscore = scores[-1]
+            maxpos = 0
+        if tscore < maxscore:
+            dets[i, :] = dets[maxpos + i + 1, :]
+            dets[maxpos + i + 1, :] = tBD
+            tBD = dets[i, :]
+
+            scores[i] = scores[maxpos + i + 1]
+            scores[maxpos + i + 1] = tscore
+            tscore = scores[i]
+
+            areas[i] = areas[maxpos + i + 1]
+            areas[maxpos + i + 1] = tarea
+            tarea = areas[i]
+
+        # IoU calculate
+        xx1 = np.maximum(dets[i, 1], dets[pos:, 1])
+        yy1 = np.maximum(dets[i, 0], dets[pos:, 0])
+        xx2 = np.minimum(dets[i, 3], dets[pos:, 3])
+        yy2 = np.minimum(dets[i, 2], dets[pos:, 2])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[pos:] - inter)
+
+        # Three methods: 1.linear 2.gaussian 3.original NMS
+        if method == 1:  # linear
+            weight = np.ones(ovr.shape)
+            weight[ovr > Nt] = weight[ovr > Nt] - ovr[ovr > Nt]
+        elif method == 2:  # gaussian
+            weight = np.exp(-(ovr * ovr) / sigma)
+        else:  # original NMS
+            weight = np.ones(ovr.shape)
+            weight[ovr > Nt] = 0
+
+        scores[pos:] = weight * scores[pos:]
+
+    # select the boxes and keep the corresponding indexes
+    inds = dets[:, 4][scores > thresh]
+    keep = inds.astype(int)
+    print(keep)
+
+    return keep
+
+def nms_result(json_result):
+    boxes  = []
+    boxscores = []
+    for result in json_result['results']:
+        boxes.append([int(result['bbox'][1]),int(result['bbox'][0]),int(result['bbox'][1])+int(result['bbox'][3]),int(result['bbox'][0])+int(result['bbox'][2])])
+        boxscores.append(result['score'])
+    boxes = np.array(boxes,dtype = np.float32)
+    boxscores = np.array(boxscores,dtype = np.float32)
+    index = py_cpu_softnms(boxes, boxscores, method=3)
+    print(index)
 
 class lesion_detector():
     def __init__(self,name='DR_lesion_detector'):
@@ -54,7 +188,7 @@ class lesion_detector():
                     data['score'] = float(bboxes[i][4])
                     data['label'] = str(label+1)
                     json_result['results'].append(data)        
-
+        nms_result(json_result)
         if not show_save_dir=='':
             image = cv2.imread(img_dir)
             for result in json_result['results']:
