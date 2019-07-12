6d261344189aa05d37c2de78c8c02f02d13d74aa
qilei123 <qileimail123@gmail.com>
2019-07-12 12:28:03 -0400

    commit all

--- configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1_second_round.py
+++ configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1_second_round.py
@@ -94,7 +94,7 @@ test_cfg = dict(
 dataset_type = 'CocoDataset'
 data_root = '/data0/qilei_chen/AI_EYE/BostonAI4DB7/'
 img_norm_cfg = dict(
-    mean=[122.5, 122.5, 122.5], std=[122.5, 122.5, 122.5], to_rgb=True)
+    mean=[127.5, 127.5, 127.5], std=[127.5, 127.5, 127.5], to_rgb=True)
 img_scale_ = (2048, 2048)
 data = dict(
     imgs_per_gpu=1,
@@ -157,5 +157,5 @@ dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = data_root+'work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_smallset_advance_optdataset4_head_v1_second_round'
 load_from = None
-resume_from = None#work_dir+'/epoch_2.pth'#'/data0/qilei_chen/AI_EYE/BostonAI4DB9/work_dirs/faster_rcnn_r50_fpn_1x/epoch_2.pth'
+resume_from = work_dir+'/epoch_3.pth'#'/data0/qilei_chen/AI_EYE/BostonAI4DB9/work_dirs/faster_rcnn_r50_fpn_1x/epoch_2.pth'
 workflow = [('train', 1)]
