473d64e377ada870a7460a95b99a7aa40e62ebf8
qilei123 <qileimail123@gmail.com>
2019-07-17 01:46:23 -0400

    commit all

--- mmdet/core/anchor/anchor_target.py
+++ mmdet/core/anchor/anchor_target.py
@@ -47,7 +47,8 @@ def anchor_target(anchor_list,
         gt_bboxes_ignore_list = [None for _ in range(num_imgs)]
     if gt_labels_list is None:
         gt_labels_list = [None for _ in range(num_imgs)]
-
+    print(gt_bboxes_list)
+    print(pseudo_bboxes)
     (all_labels, all_label_weights, all_bbox_targets, all_bbox_weights,
      pos_inds_list, neg_inds_list) = multi_apply(
          anchor_target_single,
