5d6a7d2999afece868d9158df79e4eb7a903bd3b
qilei123 <qileimail123@gmail.com>
2019-07-17 14:44:09 -0400

    commit all

--- run_sh.py
+++ run_sh.py
@@ -1,15 +1,20 @@
 import os
 import time
 
-while True:
-    os.system('clear')
-    time.sleep(1)
-    result = os.popen('nvidia-smi').read()
-    print(result)
-    lines = result.split('\n')
-    gpu_info_line = lines[8]
-    print(gpu_info_line)
-    infos = gpu_info_line.split(' ')
-    print(int(infos[17][:-3]))
-    print(infos)
-    
+def single_gpu_check_and_wait(gpu_id,memory_limit):
+    while True:
+        #os.system('clear')
+        time.sleep(1)
+        result = os.popen('nvidia-smi').read()
+        #print(result)
+        lines = result.split('\n')
+        gpu_info_line = lines[8+gpu_id*3]
+        #print(gpu_info_line)
+        infos = gpu_info_line.split(' ')
+        memory_use = int(infos[17][:-3])
+        print(int(infos[17][:-3]))
+        #print(infos)
+        if memory_use<memory_limit:
+            break
+
+single_gpu_check_and_wait(0,5000)
