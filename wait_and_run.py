e4245d7c6bd343567385fad8a93b0a92772f0ed9
qilei123 <qileimail123@gmail.com>
2019-07-17 16:00:18 -0400

    commit all

--- wait_and_run.py
+++ wait_and_run.py
@@ -30,7 +30,7 @@ def single_gpu_check_and_wait(gpu_id,memory_limit):
         if memory_use<memory_limit:
             break
 
-single_gpu_check_and_wait(1,50000)
+single_gpu_check_and_wait(1,0)
 
 command = 'sh test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1_InstanceBalancedPosSampler.sh'
 
