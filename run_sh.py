5b3d0cdb5d607036a8b88fa230f1cf705c1e973c
qilei123 <qileimail123@gmail.com>
2019-07-17 14:35:22 -0400

    commit all

--- run_sh.py
+++ run_sh.py
@@ -1,6 +1,12 @@
 import os
-result = os.popen('nvidia-smi').read()
-print(result)
-lines = result.split('\n')
-for line in lines:
-    print(line)
+import time
+
+while True:
+    time.sleep(1)
+    result = os.popen('nvidia-smi').read()
+    print(result)
+    lines = result.split('\n')
+    gpu_info_line = lines[8]
+    print(gpu_info_line)
+    infos = gpu_info_line.split(' ')
+    print(infos)
