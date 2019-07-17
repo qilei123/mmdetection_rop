488da2dfe79b6b41071a239189156df2d8179cbd
qilei123 <qileimail123@gmail.com>
2019-07-17 00:32:12 -0400

    commit all

--- tools/train.py
+++ tools/train.py
@@ -76,9 +76,9 @@ def main():
 
     model = build_detector(
         cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
-
+    print(cfg.data.train)
     train_dataset = get_dataset(cfg.data.train)
-    print(model)
+    #print(model)
     train_detector(
         model,
         train_dataset,
