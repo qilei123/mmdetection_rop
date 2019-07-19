edce854fe4b52d98a6d3c416a46c4e23bb17c4c9
qilei123 <qileimail123@gmail.com>
2019-07-19 16:52:21 -0400

    commit all

--- draw_loss_plot.py
+++ draw_loss_plot.py
@@ -36,4 +36,4 @@ def draw_loss_plot(count,loss_records):
 
 loss_record_dir='/data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_2000_v2/20190719_114750.log'
 count,loss_records = parse_loss_record(loss_record_dir)
-#draw_loss_plot(count,loss_records)
+draw_loss_plot(count,loss_records)
