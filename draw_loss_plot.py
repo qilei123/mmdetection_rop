12c2fe6c0c73d1fa029f0907c23e71be0b0ad9b0
qilei123 <qileimail123@gmail.com>
2019-07-19 17:20:01 -0400

    commit all

--- draw_loss_plot.py
+++ draw_loss_plot.py
@@ -41,7 +41,7 @@ def start_loss_plot_server(loss_record_dir,updata_time):
     while True:
         time.sleep(updata_time)
         #loss_record_dir='/data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_2000_v2/20190719_114750.log'
-        count,loss_records = parse_loss_record(loss_record_dir)
+        count,loss_records,_ = parse_loss_record(loss_record_dir)
         draw_loss_plot(count,loss_records,'')
 
 loss_record_dir='/data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_smallset_advance_optdataset4_head_v1_second_round_v2/20190717_022640.log'
