6241add654684187b961be1baf85326f7e1a431a
qilei123 <qileimail123@gmail.com>
2019-07-19 16:58:28 -0400

    commit all

--- draw_loss_plot.py
+++ draw_loss_plot.py
@@ -28,8 +28,8 @@ def draw_loss_plot(count,loss_records):
     for key in loss_records:
         x = range(count)
         y = loss_records[key]
-        plt.plot(x,y,label=key)
-        print(key)
+        plt.plot(x,y)
+        plt.lines[-1].set_label(key)
     plt.xlabel("count")
     plt.ylabel("loss")
     plt.title("A simple loss plot")
