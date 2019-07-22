3aefffc666f1dea7aa209480de5678dbd7431f05
qilei123 <qileimail123@gmail.com>
2019-07-22 13:35:37 -0400

    commit all

--- lesion_detector_api.py
+++ lesion_detector_api.py
@@ -262,8 +262,8 @@ class lesion_detector():
                 cv2.rectangle(image,(bbox[0],bbox[1]),(bbox[0]+bbox[2],bbox[1]+bbox[3]),color,2)
                 cv2.putText(image,str(result['label']),(bbox[0]+bbox[2],bbox[1]),cv2.FONT_HERSHEY_SIMPLEX, 1,color,2,cv2.LINE_AA)                
             cv2.imwrite(show_save_dir,image)
-            #cv2.imshow('test',image)
-            #cv2.waitKey(0)
+            cv2.imshow('test',image)
+            cv2.waitKey(0)
         self.json_result = json_result
         return self.json_result
     def getResult(self):
@@ -272,11 +272,11 @@ class lesion_detector():
         return self.name
 import glob
 
-DEPLOY=False
+DEPLOY_TEST=False
 
 def test():
     LesionDetector = lesion_detector()
-    if DEPLOY:
+    if DEPLOY_TEST:
         config_dir = '/home/intellifai/docker_images/mmdetection4dr/configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1.py'
         model_dir = '/home/intellifai/docker_images/mmdetection_models/epoch_9.pth'
         img_dir = '/home/intellifai/docker_images/mmdetection_models/test_data/val2014/*.jpg'
