cf3d6f7c350038847f27eb85058573f6ce53cd7f
qilei123 <qileimail123@gmail.com>
2019-07-23 17:17:04 -0400

    commit all

--- lesion_detector_api.py
+++ lesion_detector_api.py
@@ -228,12 +228,12 @@ class lesion_detector():
         self.cfg = None
         self.model = None
         self.threshold = 0.0001
-    def init_predictor(self,config_dir='/home/intellifai/docker_images/mmdetection4dr/configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1.py',model_dir='/home/intellifai/docker_images/mmdetection_models/epoch_9.pth'):
+    def init_predictor(self,config_dir='/home/ubuntu/qilei_chen/mmdetection_rop/configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1.py',model_dir='/home/ubuntu/qilei_chen/mmdetection_rop/mmdetection_models/epoch_9.pth'):
         self.cfg = mmcv.Config.fromfile(config_dir)
         self.cfg.model.pretrained = None
         self.model = build_detector(self.cfg.model, test_cfg=self.cfg.test_cfg)
         _ = load_checkpoint(self.model, model_dir)
-    def prediction(self,img_dir,show_save_dir='/home/intellifai/docker_images/mmdetection_models/test_pytorch_detector.jpg'):
+    def prediction(self,img_dir,show_save_dir='/home/ubuntu/Code/AI_4_Retinaimage/result_img/pytorch_detector_result.jpg'):
         img = mmcv.imread(img_dir)
         result = inference_detector(self.model, img, self.cfg)
         if isinstance(result, tuple):
@@ -277,10 +277,10 @@ DEPLOY_TEST=False
 def test():
     LesionDetector = lesion_detector()
     if DEPLOY_TEST:
-        config_dir = '/home/intellifai/docker_images/mmdetection4dr/configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1.py'
-        model_dir = '/home/intellifai/docker_images/mmdetection_models/epoch_9.pth'
-        img_dir = '/home/intellifai/docker_images/mmdetection_models/test_data/val2014/*.jpg'
-        show_save_dir = '/home/intellifai/docker_images/mmdetection_models/test_pytorch_detector.jpg'
+        config_dir = '/home/ubuntu/qilei_chen/mmdetection_rop/configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1.py'
+        model_dir = '/home/ubuntu/qilei_chen/mmdetection_rop/mmdetection_models/epoch_9.pth'
+        img_dir = '/home/ubuntu/qilei_chen/mmdetection_rop/mmdetection_models/test_data/val2014/*.jpg'
+        show_save_dir = '/home/ubuntu/qilei_chen/mmdetection_rop/mmdetection_models/test_pytorch_detector.jpg'
     else:
         config_dir = '/data0/qilei_chen/Development/mmdetection_rop/configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1.py'
         model_dir = '/data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_smallset_advance_optdataset4_deephead_v1/epoch_9.pth'
