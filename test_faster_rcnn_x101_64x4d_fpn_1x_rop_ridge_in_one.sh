ca0d80423b0871b8b2ded0831049aebb8fc453e3
qilei123 <qileimail123@gmail.com>
2019-07-23 12:11:57 -0400

    commit all

---
+++ test_faster_rcnn_x101_64x4d_fpn_1x_rop_ridge_in_one.sh
@@ -0,0 +1,21 @@
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64
+export CUDA_VISIBLE_DEVICES=0
+
+config_dir=configs/faster_rcnn_rop/faster_rcnn_x101_64x4d_fpn_1x_ridge_in_one.py
+model_dir=/data0/qilei_chen/AI_EYE/ROP_DATASET/9LESIONS/work_dirs/faster_rcnn_r50_fpn_1x_rop_ridge_in_one
+
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_3.pth --gpus 1 --out ${model_dir}/e3_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_4.pth --gpus 1 --out ${model_dir}/e4_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_5.pth --gpus 1 --out ${model_dir}/e5_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_6.pth --gpus 1 --out ${model_dir}/e6_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_7.pth --gpus 1 --out ${model_dir}/e7_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_8.pth --gpus 1 --out ${model_dir}/e8_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_9.pth --gpus 1 --out ${model_dir}/e9_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_10.pth --gpus 1 --out ${model_dir}/e10_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_11.pth --gpus 1 --out ${model_dir}/e11_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_12.pth --gpus 1 --out ${model_dir}/e12_results.pkl --eval bbox
+#python3 tools/test.py ${config_dir} ${model_dir}/epoch_13.pth --gpus 1 --out ${model_dir}/e13_results.pkl --eval bbox
+#python3 tools/test.py ${config_dir} ${model_dir}/epoch_14.pth --gpus 1 --out ${model_dir}/e14_results.pkl --eval bbox
+#python3 tools/test.py ${config_dir} ${model_dir}/epoch_15.pth --gpus 1 --out ${model_dir}/e15_results.pkl --eval bbox
+
+#python3 tools/test.py ${config_dir} ${model_dir}/mask_epoch_12.pth --gpus 1 --out ${model_dir}/e12_mask_results.pkl --eval bbox
