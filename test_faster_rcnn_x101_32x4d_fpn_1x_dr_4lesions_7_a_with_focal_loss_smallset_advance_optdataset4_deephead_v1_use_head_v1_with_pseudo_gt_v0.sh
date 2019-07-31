53be67e496d55ae63c993858eaa21bac0fb157db
qilei123 <qileimail123@gmail.com>
2019-07-31 17:45:37 -0400

    commit all

--- test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1_use_head_v1_with_pseudo_gt_v0.sh
+++ test_faster_rcnn_x101_32x4d_fpn_1x_dr_4lesions_7_a_with_focal_loss_smallset_advance_optdataset4_deephead_v1_use_head_v1_with_pseudo_gt_v0.sh
@@ -5,11 +5,11 @@ config_dir=configs/faster_rcnn_dr_4lesions/faster_rcnn_x101_32x4d_fpn_1x_dr_4les
 model_dir=/data0/qilei_chen/AI_EYE/BostonAI4DB7/work_dirs/faster_rcnn_r50_fpn_1x_with_focal_loss_smallset_advance_optdataset4_head_v1_with_pseudo_gt_v0
 
 #python3 tools/test.py ${config_dir} ${model_dir}/epoch_3.pth --gpus 1 --out ${model_dir}/e3_results.pkl --eval bbox
-python3 tools/test.py ${config_dir} ${model_dir}/epoch_4.pth --gpus 1 --out ${model_dir}/e4_results.pkl --eval bbox --show
+#python3 tools/test.py ${config_dir} ${model_dir}/epoch_4.pth --gpus 1 --out ${model_dir}/e4_results.pkl --eval bbox
 #python3 tools/test.py ${config_dir} ${model_dir}/epoch_5.pth --gpus 1 --out ${model_dir}/e5_results.pkl --eval bbox
 #python3 tools/test.py ${config_dir} ${model_dir}/epoch_6.pth --gpus 1 --out ${model_dir}/e6_results.pkl --eval bbox
 #python3 tools/test.py ${config_dir} ${model_dir}/epoch_7.pth --gpus 1 --out ${model_dir}/e7_results.pkl --eval bbox
-#python3 tools/test.py ${config_dir} ${model_dir}/epoch_8.pth --gpus 1 --out ${model_dir}/e8_results.pkl --eval bbox
+python3 tools/test.py ${config_dir} ${model_dir}/epoch_8.pth --gpus 1 --out ${model_dir}/e8_results.pkl --eval bbox --show
 #python3 tools/test.py ${config_dir} ${model_dir}/epoch_9.pth --gpus 1 --out ${model_dir}/e9_results.pkl --eval bbox
 #python3 tools/test.py ${config_dir} ${model_dir}/epoch_10.pth --gpus 1 --out ${model_dir}/e10_results.pkl --eval bbox
 #python3 tools/test.py ${config_dir} ${model_dir}/epoch_11.pth --gpus 1 --out ${model_dir}/e11_results.pkl --eval bbox
